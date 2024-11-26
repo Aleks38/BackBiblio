8f847a3f11ba93d3915ae20fb412ea1280cfb8a3
Aleks <alexy.dasilva@icloud.com>
2024-11-26 12:15:16 +0100

    feat[swagger]: OpenAPI

--- src/main/java/PROJET/AL/BIBLIO/demo/entity/Notification.java
+++ src/main/java/PROJET/AL/BIBLIO/demo/entity/Notification.java
@@ -4,12 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity
 @Data
 @AllArgsConstructor
--- src/main/java/PROJET/AL/BIBLIO/demo/entity/Utilisateur.java
+++ src/main/java/PROJET/AL/BIBLIO/demo/entity/Utilisateur.java
@@ -1,13 +1,10 @@
 package PROJET.AL.BIBLIO.demo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
 @Entity
 @Data
 @AllArgsConstructor
@@ -21,5 +18,8 @@ public class Utilisateur {
     private String prenom;
     private String email;
     private String motDePasse;
-    private int roleId;
+    
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
