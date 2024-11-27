7f771544782ccc330b83ce8f04a3defab794d523
animaSPW <94543307+animaSPW@users.noreply.github.com>
2024-11-27 10:03:47 +0100

    Observer pattern implemented

---
+++ src/main/java/PROJET/AL/BIBLIO/demo/Observer/ObserverLibrarian.java
@@ -0,0 +1,36 @@
+package PROJET.AL.BIBLIO.demo.Observer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ObserverLibrarian implements Observer {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    private final String librarianEmail = "hammoudimohamedanis@gmail.com"; // Replace with the librarian's email address
+
+    @Override
+    public void update(String message) {
+        if (message.contains("New Demande")) {
+            sendEmailNotification();
+        }
+    }
+
+    private void sendEmailNotification() {
+        try {
+            SimpleMailMessage messageee = new SimpleMailMessage();
+
+            messageee.setTo(librarianEmail);
+            messageee.setSubject("New Borrow Request Notification");
+            messageee.setText("Dear Librarian, you have a new borrow request.\n\nThank you.");
+            mailSender.send(messageee);
+            System.out.println("Email sent to librarian: " + librarianEmail);
+        } catch (Exception e) {
+            System.err.println("Failed to send email: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/PROJET/AL/BIBLIO/demo/ProjetAlBiblioApplication.java
+++ src/main/java/PROJET/AL/BIBLIO/demo/ProjetAlBiblioApplication.java
@@ -2,12 +2,30 @@ package PROJET.AL.BIBLIO.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import PROJET.AL.BIBLIO.demo.Observer.AppInitializer;
+import PROJET.AL.BIBLIO.demo.Observer.ObserverLibrarian;
+import PROJET.AL.BIBLIO.demo.Observer.ObserverMember;
+import PROJET.AL.BIBLIO.demo.Observer.Sujet;
 
 @SpringBootApplication
 public class ProjetAlBiblioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetAlBiblioApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ProjetAlBiblioApplication.class, args);
+
+        ObserverLibrarian observerLibrarian = context.getBean(ObserverLibrarian.class);
+        ObserverMember observerMember = context.getBean(ObserverMember.class);
+        Sujet sujet = context.getBean(Sujet.class);
+
+        AppInitializer appInitializer = new AppInitializer(sujet, observerMember, observerLibrarian);
+
+        try {
+            appInitializer.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
